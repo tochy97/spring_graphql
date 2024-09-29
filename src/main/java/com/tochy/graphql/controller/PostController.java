8167812a924d810a5e50a05d724bb5db7a91d1ff
tochy97 <tochyegeonu97@gmail.com>
2024-09-29 07:47:24 -0500

    I am stupid

--- src/main/java/com/tochy/graphql/controller/PostController.java
+++ src/main/java/com/tochy/graphql/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.tochy.graphql.controller;
 
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -21,10 +22,9 @@ public class PostController {
         return postRepository.findAll();
     }
 
-    @QueryMapping    
+    @MutationMapping    
     public Post createPost(@Argument Integer id, @Argument String title, @Argument String category, @Argument String data) {
         Post post = new Post(id, title, category, data);
-
         postRepository.save(post);
 
         return post;
@@ -32,7 +32,6 @@ public class PostController {
 
     @QueryMapping
     public String greeting(@Argument String name) {
-        System.out.println("\n\nrequest-> " + name);
         return "Hello" + name;
     }
 }
